f6bde6f38d325e5f7d2249634228c9975353039d
pzhang <pzhang@xogrp.com>
2018-10-09 14:24:52 +0800

    [New Problem AC](ContainerWithMostWater)\n[Promblem: Have no idea](RegularExpressionMatching)

---
+++ src/ContainerWithMostWater.java
@@ -0,0 +1,40 @@
+/**
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+ * <p>
+ * Note: You may not slant the container and n is at least 2.
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+ * <p>
+ * <p>
+ * <p>
+ * Example:
+ * <p>
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ */
+public class ContainerWithMostWater {
+
+    public static int maxArea(int[] height) {
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int bottomLength = j - i;
+                int area = bottomLength * (height[i] <= height[j] ? height[i] : height[j]);
+                if(maxArea < area) {
+                    maxArea = area;
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(arr));
+    }
+
+}
--- src/RegularExpressionMatching.java
+++ src/RegularExpressionMatching.java
@@ -1,47 +1,47 @@
 /**
  * Link: https://leetcode.com/problems/regular-expression-matching/
  * Status:
- *
+ * <p>
  * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
- *
+ * <p>
  * '.' Matches any single character.
  * '*' Matches zero or more of the preceding element.
  * The matching should cover the entire input string (not partial).
- *
+ * <p>
  * Note:
- *
+ * <p>
  * s could be empty and contains only lowercase letters a-z.
  * p could be empty and contains only lowercase letters a-z, and characters like . or *.
  * Example 1:
- *
+ * <p>
  * Input:
  * s = "aa"
  * p = "a"
  * Output: false
  * Explanation: "a" does not match the entire string "aa".
  * Example 2:
- *
+ * <p>
  * Input:
  * s = "aa"
  * p = "a*"
  * Output: true
  * Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
  * Example 3:
- *
+ * <p>
  * Input:
  * s = "ab"
  * p = ".*"
  * Output: true
  * Explanation: ".*" means "zero or more (*) of any character (.)".
  * Example 4:
- *
+ * <p>
  * Input:
  * s = "aab"
  * p = "c*a*b"
  * Output: true
  * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
  * Example 5:
- *
+ * <p>
  * Input:
  * s = "mississippi"
  * p = "mis*is*p*."
@@ -49,11 +49,38 @@
  */
 public class RegularExpressionMatching {
 
-    public boolean isMatch(String s, String p) {
+    public static boolean isMatch(String s, String p) {
+        String[] coverts = convert(s, p, '/');
+        String s1 = coverts[0];
+        String p1 = coverts[1];
         return false;
     }
 
-    public static void main(String[] args) {
+    private static String charArrayToString(char[] charArray, char ignore) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : charArray) {
+            if (c != ignore) {
+                stringBuilder.append(c);
+            }
+        }
+        return stringBuilder.toString();
+    }
 
+    private static String[] convert(String s, String p, final char deleteType) {
+        int si = 0;
+        int pj = 0;
+        char[] sCharArray = s.toCharArray();
+        char[] pCharArray = p.toCharArray();
+        while (si < sCharArray.length && pj < pCharArray.length) {
+
+        }
+        String s1 = charArrayToString(sCharArray, deleteType);
+        String p1 = charArrayToString(pCharArray, deleteType);
+        return new String[]{s1, p1};
+    }
+
+    public static void main(String[] args) {
+        System.out.println("s1 = mississippi\np1 = mis*is*p*.");
+        isMatch("mississippi","mis*is*p*.");
     }
 }
