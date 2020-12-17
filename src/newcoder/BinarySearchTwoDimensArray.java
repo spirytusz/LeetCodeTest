ca78f0b875a0f6b7da655e4ad5758a595a701e12
zkw012300 <zhangwel261717@gmail.com>
2020-12-17 22:22:43 +0800

    漏提交代码

--- src/newcoder/BinarySearchTwoDimensArray.java
+++ src/newcoder/BinarySearchTwoDimensArray.java
@@ -1,6 +1,9 @@
 package newcoder;
 
 /**
+ * Link: https://www.nowcoder.com/practice/abc3fe2ce8e146608e868a70efebf62e?tpId=13&&tqId=11154&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ * Status: AC
+ *
  * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
  * 每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一
  * 个整数，判断数组中是否含有该整数。
