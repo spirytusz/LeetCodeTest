c7f4f8aa83448b49b3a312ce92c320348c058b08
spirytusz <zhangwel261717@gmail.com>
2025-04-01 21:48:44 +0800

    [Problem AC](30. Substring with Concatenation of All Words)

--- src/SubStringWithConcatenationOfAllWords.java
+++ src/SubStringWithConcatenationOfAllWords.java
@@ -1,80 +1,124 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * Link: https://leetcode.com/problems/substring-with-concatenation-of-all-words/
- * Status: Have no idea
+ * <a href="https://leetcode.com/problems/substring-with-concatenation-of-all-words/description/">30. Substring with Concatenation of All Words</a>
  *
- * You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
- * <p>
- * Example 1:
- * <p>
- * Input:
- * s = "barfoothefoobarman",
- * words = ["foo","bar"]
- * Output: [0,9]
- * Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar" respectively.
- * The output order does not matter, returning [9,0] is fine too.
- * Example 2:
- * <p>
- * Input:
- * s = "wordgoodstudentgoodword",
- * words = ["word","student"]
- * Output: []
+ * <p>You are given a string <code>s</code> and an array of strings <code>words</code>. All the strings of <code>words</code> are of <strong>the same length</strong>.</p>
+ *
+ * <p>A <strong>concatenated string</strong> is a string that exactly contains all the strings of any permutation of <code>words</code> concatenated.</p>
+ *
+ * <ul>
+ * 	<li>For example, if <code>words = [&quot;ab&quot;,&quot;cd&quot;,&quot;ef&quot;]</code>, then <code>&quot;abcdef&quot;</code>, <code>&quot;abefcd&quot;</code>, <code>&quot;cdabef&quot;</code>, <code>&quot;cdefab&quot;</code>, <code>&quot;efabcd&quot;</code>, and <code>&quot;efcdab&quot;</code> are all concatenated strings. <code>&quot;acdbef&quot;</code> is not a concatenated string because it is not the concatenation of any permutation of <code>words</code>.</li>
+ * </ul>
+ *
+ * <p>Return an array of <em>the starting indices</em> of all the concatenated substrings in <code>s</code>. You can return the answer in <strong>any order</strong>.</p>
+ *
+ * <p>&nbsp;</p>
+ * <p><strong class="example">Example 1:</strong></p>
+ *
+ * <div class="example-block">
+ * <p><strong>Input:</strong> <span class="example-io">s = &quot;barfoothefoobarman&quot;, words = [&quot;foo&quot;,&quot;bar&quot;]</span></p>
+ *
+ * <p><strong>Output:</strong> <span class="example-io">[0,9]</span></p>
+ *
+ * <p><strong>Explanation:</strong></p>
+ *
+ * <p>The substring starting at 0 is <code>&quot;barfoo&quot;</code>. It is the concatenation of <code>[&quot;bar&quot;,&quot;foo&quot;]</code> which is a permutation of <code>words</code>.<br />
+ * The substring starting at 9 is <code>&quot;foobar&quot;</code>. It is the concatenation of <code>[&quot;foo&quot;,&quot;bar&quot;]</code> which is a permutation of <code>words</code>.</p>
+ * </div>
+ *
+ * <p><strong class="example">Example 2:</strong></p>
+ *
+ * <div class="example-block">
+ * <p><strong>Input:</strong> <span class="example-io">s = &quot;wordgoodgoodgoodbestword&quot;, words = [&quot;word&quot;,&quot;good&quot;,&quot;best&quot;,&quot;word&quot;]</span></p>
+ *
+ * <p><strong>Output:</strong> <span class="example-io">[]</span></p>
+ *
+ * <p><strong>Explanation:</strong></p>
+ *
+ * <p>There is no concatenated substring.</p>
+ * </div>
+ *
+ * <p><strong class="example">Example 3:</strong></p>
+ *
+ * <div class="example-block">
+ * <p><strong>Input:</strong> <span class="example-io">s = &quot;barfoofoobarthefoobarman&quot;, words = [&quot;bar&quot;,&quot;foo&quot;,&quot;the&quot;]</span></p>
+ *
+ * <p><strong>Output:</strong> <span class="example-io">[6,9,12]</span></p>
+ *
+ * <p><strong>Explanation:</strong></p>
+ *
+ * <p>The substring starting at 6 is <code>&quot;foobarthe&quot;</code>. It is the concatenation of <code>[&quot;foo&quot;,&quot;bar&quot;,&quot;the&quot;]</code>.<br />
+ * The substring starting at 9 is <code>&quot;barthefoo&quot;</code>. It is the concatenation of <code>[&quot;bar&quot;,&quot;the&quot;,&quot;foo&quot;]</code>.<br />
+ * The substring starting at 12 is <code>&quot;thefoobar&quot;</code>. It is the concatenation of <code>[&quot;the&quot;,&quot;foo&quot;,&quot;bar&quot;]</code>.</p>
+ * </div>
+ *
+ * <p>&nbsp;</p>
+ * <p><strong>Constraints:</strong></p>
+ *
+ * <ul>
+ * 	<li><code>1 &lt;= s.length &lt;= 10<sup>4</sup></code></li>
+ * 	<li><code>1 &lt;= words.length &lt;= 5000</code></li>
+ * 	<li><code>1 &lt;= words[i].length &lt;= 30</code></li>
+ * 	<li><code>s</code> and <code>words[i]</code> consist of lowercase English letters.</li>
+ * </ul>
  */
 public class SubStringWithConcatenationOfAllWords {
 
-    static HashMap<Integer, String> mapper = new HashMap<>();
 
     public static List<Integer> findSubstring(String s, String[] words) {
         List<Integer> result = new ArrayList<>();
-        if (words.length != 0) {
-            String[][] wordss = new String[words.length][];
-            for (int i = 0; i < words.length; i++) {
-                wordss[i] = words;
+        if (s == null || s.isEmpty() || words == null || words.length == 0) {
+            return result;
+        }
+
+        int wordLength = words[0].length();
+        int numWords = words.length;
+
+        Map<String, Integer> wordFreq = new HashMap<>();
+        for (String word : words) {
+            wordFreq.put(word, wordFreq.getOrDefault(word, 0) + 1);
+        }
+
+        for (int i = 0; i < wordLength; i++) {
+            int left = i;
+            int right = i;
+            int count = 0;
+            Map<String, Integer> windowMap = new HashMap<>();
+
+            while (right + wordLength <= s.length()) {
+                String rightWord = s.substring(right, right + wordLength);
+                right += wordLength;
+                if (wordFreq.containsKey(rightWord)) {
+                    windowMap.put(rightWord, windowMap.getOrDefault(rightWord, 0) + 1);
+                    count++;
+
+                    while (windowMap.get(rightWord) > wordFreq.get(rightWord)) {
+                        String leftWord = s.substring(left, left + wordLength);
+                        left += wordLength;
+                        windowMap.put(leftWord, windowMap.get(leftWord) - 1);
+                        count--;
+                    }
+
+                    if (numWords == count) {
+                        result.add(left);
+                    }
+                } else {
+                    count = 0;
+                    windowMap.clear();
+                    left = right;
+                }
             }
-            forceSearch(result, new String[words.length], s, wordss, new ArrayList<>(), 0);
         }
         return result;
     }
 
-    private static String stringArrayToString(String[] strings) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String string : strings) {
-            stringBuilder.append(string);
-        }
-        return stringBuilder.toString();
-    }
-
-    private static void forceSearch(List<Integer> result, String[] item, String s, String[][] wordss, List<String> indexMemory, int depth) {
-        if (depth < wordss.length) {
-            for (int i = 0; i < wordss[depth].length; i++) {
-                String index = i + "";
-                if (!indexMemory.contains(index)) {
-                    indexMemory.add(index);
-                    item[depth] = wordss[depth][i];
-                    forceSearch(result, item, s, wordss, indexMemory, depth + 1);
-                    item[depth] = null;
-                    indexMemory.remove(index);
-                }
-            }
-        } else {
-            String str = stringArrayToString(item);
-            for (int i = 0; i < s.length(); i++) {
-                int index = s.substring(i, s.length()).indexOf(str);
-                if (index != -1 && !result.contains(index + i)) {
-                    result.add(index + i);
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        findSubstring("pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel",
-                new String[]{"dhvf", "sind", "ffsl", "yekr", "zwzq", "kpeo", "cila", "tfty", "modg", "ztjg", "ybty", "heqg", "cpwo", "gdcj", "lnle", "sefg", "vimw", "bxcb"});
-        System.out.println((System.currentTimeMillis() - start) + "ms");
+        System.out.println(findSubstring("barfoothefoobarman", new String[]{"foo", "bar"}));
+        System.out.println(findSubstring("wordgoodgoodgoodbestword", new String[]{"word", "good", "best", "word"}));
+        System.out.println(findSubstring("barfoofoobarthefoobarman", new String[]{"bar", "foo", "the"}));
     }
-
 }
